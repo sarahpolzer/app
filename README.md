366cd3d84963f412f49d7555f6b684f16a62a272
Sarah <spolzer@masonlive.gmu.edu>
2019-09-14 13:02:12 -0700

    My bigrams app!

---
+++ README.md
@@ -0,0 +1,39 @@
+# **Bigrams Application **
+
+## Sarah Polzer
+
+Takes in string or a file name on a web application and returns a bar chart of the Bigrams in the text. Runs on Python 3.
+
+### Dependencies
+
+Flask==1.1.1
+nltk==3.4.5
+plotly==4.1.1
+
+Dependencies can be installed with pip3. Installation instruction are here: https://pip.pypa.io/en/stable/installing/. I'm using Ubuntu, so I just installed pip3 with apt-get.
+
+```bash
+sudo apt-get install python3-pip
+```
+
+Once pip3 is installed, dependencies can be installed from the requirements.txt file. The command for installation is below.
+
+```bash
+pip3 install -U -r requirements.txt
+```
+
+### **Run the Application**
+
+Go into the app folder and run 
+
+```bash
+python3 bigrams_app.py
+```
+
+A home page should launch
+
+![](images/home_page.png)
+
+Click submit and you should see a bar chart of all of the Bigrams and their counts
+
+![](images/bar_chart.png)
---
+++ bigrams_app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request
+import nltk 
+import plotly.graph_objects as go
+
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+    #Returns the html template of the homepage
+    return render_template('enter_text.html')
+
+@app.route('/', methods=['POST'])
+def enter_text():
+    #handles the text or file name entered by the user and returns the bar chart of the bigrams
+    text = request.form['text']
+    bigrams,counts = create_bigrams(text)
+    if len(bigrams)==0:
+        return  "No Bigrams Found :("
+    else:
+        bigram_bar = go.Figure([go.Bar(x=bigrams, y=counts)])
+        bigram_bar.update_layout(title_text='Bigrams and their Counts')
+        return bigram_bar.show()
+
+def create_bigrams(text):
+    #Takes the text entered by the user and returns a list of bigrams and a list of their counts
+    bigrams = []
+    counts = []
+    if len(text.split(' '))==1 and "." in text:
+        f = open(text)
+        text = f.read()
+    text = text.lower()
+    bigrm = list(nltk.bigrams(text.split()))
+    for b in list(set(bigrm)):
+        bigrams.append(b[0] + "," + b[1])
+        counts.append(bigrm.count(b))
+    return bigrams, counts
+    
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+    
